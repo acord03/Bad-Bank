701c877b82bfdd408173b26b0bef348784fa556f
acord03 <averycordero40@gmaillcom>
2022-09-30 03:48:26 -0400

    improved error message functionality on the create-account page

--- src/Routes/CreateAccount.js
+++ src/Routes/CreateAccount.js
@@ -1,6 +1,6 @@
 import '../App.css';
 import {useState, useContext} from 'react'
-import {UserContext} from '../UserContext';
+import {UserContext} from '../UserContext'
 import {Submissions} from '../Submissions'
 
 function CreateAccount(){
@@ -9,19 +9,24 @@ function CreateAccount(){
     const [username, setUsername] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [selected1, setSelected1] = useState(false)
+    const [selected2, setSelected2] = useState(false)
+    const [selected3, setSelected3] = useState(0)
+    
     
     const handleSubmit = (event)=>{
         event.preventDefault();
         let newAcc = {
-            user: username,
-            adress: email,
-            pass: password,
+            username: username,
+            email: email,
+            password: password,
             balance: 100
         }
         let newSubmission = submissions.concat(newAcc);
         setSubmissions(newSubmission)
         const newArr = value.concat(newAcc)
         setValue(newArr)
+        alert('New Account Created')
     }
     
     return(
@@ -30,23 +35,28 @@ function CreateAccount(){
                 <h1 className="card-header bg-dark">Account Creation</h1> 
                 <form onSubmit={handleSubmit}>
                     <input type="text" id="username" className="inner-input" onChange={(e)=>{
+                        setSelected1(true)
                         setUsername(e.target.value)
                         console.log(username)
                     }} placeholder="Username"/>
                     <br/>
-                    {username.length === 0 && <p className="error">This field is required</p>}
+                    {username.length === 0 && selected1 ===true ? <p className="error">This field is required</p>: null}
                     <input type="text" id="email" className="inner-input" onChange={(e)=>{
+                        setSelected2(true)
                         setEmail(e.target.value)
                         console.log(email)
                     }} placeholder="Email"/>
                     <br/>
-                    {email.length === 0 && <p className="error">This field is required</p>}
+                    {email.length === 0 && selected2 === true ? <p className="error">This field is required</p>: null}
                     <input type="text" id="password" className="inner-input" onChange={(e)=>{
+                        let keyInputs = selected3
+                        keyInputs++
+                        setSelected3(keyInputs)
                         setPassword(e.target.value)
                         console.log(password)
                     }} placeholder="Password"/>
                     <br/>
-                    {password.length < 8 && <p className="error">Password must 8 characters or longer</p>}
+                    {password.length < 8 && selected3 >= 8 ? <p className="error">Password must 8 characters or longer</p>: null}
                     <button className="inner-button" id="submit-button" disabled={!username || !email || password.length < 8 ? true: false}>Create Account</button>
                 </form>
             </div>
