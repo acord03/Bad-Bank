e13b6e936da9040cbfbb77e73bad008827d4abf1
acord03 <averycordero40@gmail.com>
2022-10-25 21:33:42 -0400

    it was not a glitch I moved the location of my src folder on accident because I'm a silly silly man

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,5 +18,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-//Initializing the authentication feature
+//Initialize the authentication feature
 export const auth = getAuth(app);
+
+//Initialize Firestore database
+export const db = getFirestore(app)
